0fb4dbda34842049190c1ec60fab443da9245588
mudassir8050 <125982472+mudassir8050@users.noreply.github.com>
2023-06-29 21:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,29 @@
-//your code here
-function sortBandNames(bandNames) {
-  // Remove articles ('a', 'an', 'the') from band names for sorting
-  const regex = /^(a|an|the)\s/i;
-  const sortedBandNames = bandNames
-    .map((name) => name.replace(regex, ''))
-    .sort((a, b) => a.localeCompare(b));
+def sort_band_names(band_names):
+    # Remove articles ('a', 'an', 'the') from band names
+    articles = ['a', 'an', 'the']
+    cleaned_band_names = []
+    for name in band_names:
+        # Split the name into words
+        words = name.split()
+        # Exclude articles from the list of words
+        words = [word for word in words if word.lower() not in articles]
+        # Join the remaining words back into a string
+        cleaned_name = ' '.join(words)
+        cleaned_band_names.append(cleaned_name)
 
-  // Create the HTML list
-  const ul = document.createElement('ul');
-  ul.id = 'band';
+    # Sort the cleaned band names in lexicographic order
+    sorted_band_names = sorted(cleaned_band_names)
 
-  sortedBandNames.forEach((bandNames) => {
-    const li = document.createElement('li');
-    li.textContent = bandName;
-    ul.appendChild(li);
-  });
+    # Generate the HTML list
+    html_list = '<ul id="band">'
+    for name in sorted_band_names:
+        html_list += '<li>{}</li>'.format(name)
+    html_list += '</ul>'
 
-  // Add the list to the document body or a specific element
-  document.body.appendChild(ul);
-}
+    return html_list
 
 
-let bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones', 'Pink Floyd'];
-
-// Remove articles from band names
-let cleanBandNames = bandNames.map(name => name.replace(/^(?:the|a|an)\s+/i, ''));
-
-// Sort the clean band names in lexicographic order
-let sortedBandNames = cleanBandNames.sort((a, b) => a.localeCompare(b));
-
-// Create the HTML list
-let list = document.createElement('ul');
-list.setAttribute('id', 'band');
-
-// Add band names to the list
-sortedBandNames.forEach(name => {
-  let listItem = document.createElement('li');
-  listItem.textContent = name;
-  list.appendChild(listItem);
-});
-
-// Print the HTML list
-console.log(list.outerHTML);
+# Example usage
+band_names = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones', 'Anthrax']
+html_output = sort_band_names(band_names)
+print(html_output)
